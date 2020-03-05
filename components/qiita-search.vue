36e99d2be3dc86f9c30bc4d715debb9fc2f84c58
yukito <yukito1930@gmail.com>
2020-03-05 19:14:15 +0900

    fix qiita search function

--- components/qiita-search.vue
+++ components/qiita-search.vue
@@ -36,16 +36,33 @@
 
     <button type="button" v-on:click="doSearch">検索する</button>
     <p>検索ボックスの内容:{{ searchBoxContent }}</p>
+    <div>
+      <select v-model="sortType">     
+        <option value="relationSort">関連順</option>
+        <option value="newSort">新着順</option>
+        <option value="likeSort">いいね順</option>
+        
+      </select>   
+      <span>{{ sortChange }}</span>
+    </div>
     <table border="1">
       <thead>
         <tr>
-          <th>記事タイトル</th>                    
-        </tr>
+          <th>更新日</th>
+          <th>記事タイトル</th>
+          <th>いいね数</th>                    
+        </tr>        
       </thead>
       <tbody>                
         <tr v-for="element in allArticleDataForDisplay" v-bind:key="element.url">
           <td>
+            {{ element.updated_at.slice(0,10) }}</a>
+          </td>
+          <td>
             <a v-bind:href="element.url">{{ element.title }}</a>
+          </td>
+          <td>
+            {{ element.likes_count }}</a>
           </td>                    
         </tr>
       </tbody>
@@ -69,6 +86,9 @@ export default {
       searchBox:"",
       searchBoxContentArray:[],
       allArticleDataForDisplay:[], //表示する記事データ格納用
+      sortType: "relationSort",
+      allArticleDataSorted: [],//並べ替え後のデータ格納用
+      pageNum: 1,//ページネーションの現在ページ
 
     }
   },  
@@ -88,15 +108,52 @@ export default {
       this.searchBoxContentArray = this.searchBoxContent.split(/\s+/);        
       //console.log("this.searchBoxContentArray:");
       //console.log(this.searchBoxContentArray);
+    },
+    sortChange:function(){
+      console.log("sortChange called in watch");    
+      //Qiitaのデータをソート用配列に格納する
+      let dataStoredArray = this.allArticleDataSorted.slice(0, this.allArticleDataSorted.length);
+      //ソートを実行
+      if(this.sortType === "likeSort"){
+        dataStoredArray.sort(likesSortFunc);
+
+      } else if(this.sortType === "newSort"){
+        dataStoredArray.sort(dateSortFunc);
+
+      } else {
+        dataStoredArray = this.allArticleData.slice(0, this.allArticleData.length);
+
+      }      
+      //ソート用の関数を定義
+      function likesSortFunc(a,b){
+        return b.likes_count - a.likes_count;
+      }
+
+      function dateSortFunc(a,b){
+        return (a.updated_at < b.updated_at ? 1 : -1);
+      }      
+
+      //ソートしたデータをリアクティブな配列に格納
+      this.allArticleDataSorted.splice(-this.allArticleDataSorted.length);
+      for(let j=0; j<dataStoredArray.length; j++){
+        this.allArticleDataSorted.push(dataStoredArray[j]);
+      }       
+      this.onchange(this.pageNum);
+      //this.onchange(1);
     }
     
   },
-  computed:{          
+  computed:{
+    sortChange(){        
+      return this.sortType;
+
+    },          
   },
   methods: {   
     onchange( page ){
       //console.log("page in onchange");
       //console.log(page);
+      this.pageNum = page;
       let data = this.getRangeByPage(page);
 
       //表示用配列の初期化
@@ -108,11 +165,13 @@ export default {
     getRangeByPage( page ){
        //1ページの表示数
        const SIZE = 10;
-       return this.allArticleData.slice((page - 1) * SIZE, (page - 1) * SIZE + SIZE);
+       //return this.allArticleData.slice((page - 1) * SIZE, (page - 1) * SIZE + SIZE);
+       return this.allArticleDataSorted.slice((page - 1) * SIZE, (page - 1) * SIZE + SIZE);
     },
     async doSearch(){
       //配列の初期化
       this.allArticleData.splice(-this.allArticleData.length);
+      this.allArticleDataSorted.splice(-this.allArticleDataSorted.length);
       this.allArticleDataForDisplay.splice(-this.allArticleDataForDisplay.length);
 
       let urlParameter ='';
@@ -154,6 +213,7 @@ export default {
         }
         for(let j=0; j<result.length; j++){
           this.allArticleData.push(result[j]);
+          this.allArticleDataSorted.push(result[j]);
         }        
       }                
 
